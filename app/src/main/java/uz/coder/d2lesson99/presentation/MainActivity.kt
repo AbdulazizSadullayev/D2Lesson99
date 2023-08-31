f513d57cb9241b9a141c4fa81979026fa9551615
AbdulazizSadullayev <abdualazizdjahon@gmail.com>
2023-08-31 11:15:43 +0500

    Initial commit

---
+++ app/src/main/java/uz/coder/d2lesson99/presentation/MainActivity.kt
@@ -0,0 +1,15 @@
+package uz.coder.d2lesson99.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import uz.coder.d2lesson99.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
